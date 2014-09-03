5e327d0b52891fcfad9d9827588fccedd5af2529
unknown <602327345@BTG466255.iuser.iroot.adidom.com>
2014-09-03 15:25:23 +0200

    Improved shape definition

--- src/com/crocoware/infographix/test/GraphicTestView.java
+++ src/com/crocoware/infographix/test/GraphicTestView.java
@@ -5,7 +5,6 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AnticipateInterpolator;
 import android.view.animation.Interpolator;
 
@@ -13,7 +12,9 @@ import com.crocoware.infographix.AbstractBorderedDrawable;
 import com.crocoware.infographix.ComposedBordered;
 import com.crocoware.infographix.IBorderedDrawable;
 import com.crocoware.infographix.shapes.DownRightArcShape;
+import com.crocoware.infographix.shapes.HSegment;
 import com.crocoware.infographix.shapes.PipeShape;
+import com.crocoware.infographix.shapes.VSegment;
 
 public class GraphicTestView extends View {
 
@@ -41,19 +42,27 @@ public class GraphicTestView extends View {
 		float x = 10;
 		float y = 150;
 		float width2 = 300;
-		AbstractBorderedDrawable pipe0 = new PipeShape(x, x + width,
-				y - height, y, false);
-		AbstractBorderedDrawable pipe1 = new DownRightArcShape(x, x + width, y,
-				x + width2, y + height, y + height2);
-		AbstractBorderedDrawable pipe2 = new PipeShape(x + width2, x + width
-				+ width2, y + height, y + height2, true);
+		HSegment startH = new HSegment(x, x + width, y - height);
+		HSegment endH = new HSegment(x, x + width, y);
+
+		VSegment startV = new VSegment(x + width2, y + height, y + height2);
+		VSegment endV = new VSegment(x + width + width2, y + height, y
+				+ height2);
+
+		AbstractBorderedDrawable pipe0 = new PipeShape(startH, endH);
+		AbstractBorderedDrawable pipe1 = new DownRightArcShape(endH, startV);
+		AbstractBorderedDrawable pipe2 = new PipeShape(startV, endV);
+
 		pipe = new ComposedBordered(pipe0, pipe1, pipe2);
+
 		// mDrawable.getPaint().setColor(0xff74AC23);
 	}
 
 	protected void onDraw(Canvas canvas) {
 
 		float elapsedTime = System.currentTimeMillis() - startTime;
+		if (elapsedTime > DURATION)
+			elapsedTime = DURATION;
 		float ratio = ease.getInterpolation(elapsedTime / DURATION);
 		width = 100 + ratio * 100;
 		height = 100 + ratio * 80;
