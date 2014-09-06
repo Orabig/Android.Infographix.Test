6725247963f0f770e0181bd90f1335fdb41fa938
Benoît Chauvet <benoit.chauvet@gmail.com>
2014-09-06 22:51:53 +0200

    Major update : Added easy pipeline definition

--- src/com/crocoware/infographix/test/GraphicTestView.java
+++ src/com/crocoware/infographix/test/GraphicTestView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.Shader.TileMode;
 import android.graphics.SweepGradient;
@@ -18,11 +19,13 @@ import com.crocoware.infographix.AbstractBorderedDrawable;
 import com.crocoware.infographix.ComposedBordered;
 import com.crocoware.infographix.IBorderedDrawable;
 import com.crocoware.infographix.shapes.ArcShape;
+import com.crocoware.infographix.shapes.Arrow;
 import com.crocoware.infographix.shapes.CurvedPipeShape;
 import com.crocoware.infographix.shapes.DownRightArcShape;
 import com.crocoware.infographix.shapes.HJoinShape;
 import com.crocoware.infographix.shapes.HSegment;
 import com.crocoware.infographix.shapes.HSplitShape;
+import com.crocoware.infographix.shapes.NewPipeShape;
 import com.crocoware.infographix.shapes.PipeShape;
 import com.crocoware.infographix.shapes.Segment;
 import com.crocoware.infographix.shapes.VSegment;
@@ -109,6 +112,7 @@ public class GraphicTestView extends View {
 		float YCa4 = Y1 + gap * (ratio);
 
 		VSegment SA = new VSegment(X0, Y0, Y1);
+		VSegment SAA = new VSegment(-X0, Y0, Y1);
 		VSegment SB = new VSegment(XB, Y0, Y1);
 
 		VSegment SBa = new VSegment(XB, Y0, YB1);
@@ -123,6 +127,40 @@ public class GraphicTestView extends View {
 		pipe = new ComposedBordered(new PipeShape(SA, SB), new CurvedPipeShape(
 				SBa, SCa), new CurvedPipeShape(SBb, SCb), new PipeShape(SCa,
 				SDa), new PipeShape(SCb, SDb));
+	}
+
+	private void drawGrid(Canvas canvas, int size) {
+		Paint paint = new Paint();
+		for (int i = 0; i <= 400; i += size) {
+			canvas.drawLine(i, 0, i, 400, paint);
+			canvas.drawLine(0, i, 400, i, paint);
+		}
+	}
+
+	private void version4() {
+		int SIZE = 50;
+		ArcShape arc;
+		ArcShape arc2;
+		ArcShape arc3;
+		{
+		Segment BA = new Segment(2*SIZE,SIZE,2*SIZE,0);
+		PointF origin = new PointF(2*SIZE,2*SIZE);
+		arc = new ArcShape(BA, origin, 90+45);
+		}
+		{
+		Segment BA = new Segment(2*SIZE,SIZE,2*SIZE,0);
+		PointF origin = new PointF(2*SIZE,2*SIZE);
+		arc2 = new ArcShape(BA, origin, 90);
+		arc2.resize(0, SIZE*4,200,200);
+		}
+		{
+		Segment BA = new Segment(2*SIZE,SIZE,2*SIZE,0);
+		PointF origin = new PointF(2*SIZE,2*SIZE);
+		arc3 = new ArcShape(BA, origin, 90);
+		arc3.translate(SIZE*4, SIZE*4);
+		}
+	pipe=new ComposedBordered(arc,arc2,arc3);
+//	pipe.translate(-SIZE,0);
 	}
 
 	private void version3() {
@@ -142,8 +180,17 @@ public class GraphicTestView extends View {
 
 		// Calcul des positions
 		float Y1 = Y0 + HEIGHT;
-		float XB = X0 + pX;
+		float XB = X0 + pX*3;
 
+		Segment SAA = new Segment(X0, Y0-2* HEIGHT, X0+20,Y1- 2*HEIGHT-20);
+		Segment SBA = new Segment(XB+WIDTH, Y0-2* HEIGHT,XB+WIDTH+20, Y1-2* HEIGHT-20);
+		Segment SBC = new Segment(SBA).reverse();SBC.translate(100, 250);
+
+		NewPipeShape first = new NewPipeShape(SAA, SBA);
+		NewPipeShape after = new NewPipeShape(SBA, 150);
+		first.setInputClosed(true);
+		first.setOutputArrow(Arrow.INNER);
+		
 		VSegment SA = new VSegment(X0, Y0, Y1);
 		VSegment SB = new VSegment(XB, Y0, Y1);
 
@@ -159,39 +206,92 @@ public class GraphicTestView extends View {
 		VSegment SDadded = SDa.scaleUp(2.50f);
 		VSegment SDend = SDadded.translateV(pX, 0);
 
-		HJoinShape join = new HJoinShape(/*SDend*/SDadded, SDb, WIDTH);
+		HJoinShape join = new HJoinShape(/* SDend */SDadded, SDb, WIDTH);
 
 		VSegment joined = join.getOutputSegment();
 		VSegment last = joined.translateV(pX, 0);
-		
-		PointF center = new PointF(last.getX(),last.getY1()-pX);
-		
+
+		PointF center = new PointF(last.getX(), last.getY1() - pX);
+
 		Segment[] starts = last.split(5);
 		ArcShape arcs[] = new ArcShape[5];
-		int colors[] = {Color.CYAN,Color.RED,Color.LTGRAY,Color.GREEN,Color.BLUE};
-		for (int i=0;i<5;i++) {
-		 arcs[i] = new ArcShape(starts[i], center, -100-15*i);
-		 arcs[i].setSweepShader(Color.WHITE,colors[i]);
+		int colors[] = { Color.CYAN, Color.RED, Color.LTGRAY, Color.GREEN,
+				Color.BLUE };
+		for (int i = 0; i < 5; i++) {
+			arcs[i] = new ArcShape(starts[i], center, -100 - 15 * i);
+			arcs[i].setSweepShader(Color.WHITE, colors[i]);
 		}
-		
 
-		pipe = new ComposedBordered(new PipeShape(SA, SB), split,
-				new PipeShape(SCa, SDa), new PipeShape(SCb, SDb),
-				//				new PipeShape(SDadded,SDend),
-				join, new PipeShape(joined, last),arcs[0]
-						,arcs[1],arcs[2],arcs[3],arcs[4]
-						);
-//		Log.e("pipe",""+pipe.getLeft()+","+pipe.getTop()+","+(pipe.getRight()-pipe.getLeft())+","+(pipe.getBottom()-pipe.getTop()));
-		float range10 = (1+(float)Math.cos(elapsedTime/300))*5;
-//		pipe.resize(10+range10,20+range10,413.333f,241.666f);
-		pipe.translate(5-range10,5-range10);
+		PipeShape criPipe = new PipeShape(SCa, SDa);
+		criPipe.setText("critical");
+		pipe = new ComposedBordered(after,first, split,
+				criPipe, new PipeShape(SCb, SDb),
+				// new PipeShape(SDadded,SDend),
+				join, new PipeShape(joined, last), arcs[0], arcs[1], arcs[2],
+				arcs[3], arcs[4]);
+		pipe.setBodyColor(Color.WHITE);
+		// Log.e("pipe",""+pipe.getLeft()+","+pipe.getTop()+","+(pipe.getRight()-pipe.getLeft())+","+(pipe.getBottom()-pipe.getTop()));
+//		float range10 = (1 + (float) Math.cos(elapsedTime / 500)) * 50;
+//		 pipe.resize(10,20+range10,400,150.0f+range10);
+//		pipe.translate(5 - range10, 5 - range10);
 
 		split.setBodyShader(new LinearGradient(split.getLeft(), split.getTop(),
 				split.getRight(), split.getTop(), Color.RED, Color.BLACK,
 				TileMode.CLAMP));
 
+		pipe.setEdgeColor(Color.BLACK);
+		// arc.setEdgeColor(Color.BLACK);
+	}
+
+
+	private void version5() {
+		float WIDTH = 100;
+		 // On découpe l'espace H en bouts
+
+		// Ecartement initial
+		float HEIGHT = 50;
+		// Position du haut du funnel (entrée)
+		float X0 = 150;
+		float Y0 = 200;
+
+		float ratio = 0.4f;// 0.5f + (float) Math.cos(elapsedTime / 1000) *
+							// 0.4f;
+
+		float gap = 30;
+
+		// Calcul des positions
+		float Y1 = Y0 + HEIGHT;
+		float XB = X0 + WIDTH;
+
+		VSegment SA = new VSegment(X0, Y0, Y1);
+		VSegment SB = new VSegment(XB, Y0, Y1);
+
+		PointF center = new PointF(SB.getX(), SB.getY1() - 50);
+
+		int N=2;
+		Segment[] starts = SB.split(N);
+		ArcShape arcs[] = new ArcShape[N];
+		int colors[] = { Color.CYAN, Color.RED, Color.LTGRAY, Color.GREEN,
+				Color.BLUE };
+		for (int i = 0; i < N; i++) {
+			arcs[i] = new ArcShape(starts[i], center, -100 - 15 * i);
+			arcs[i].setSweepShader(Color.WHITE, colors[i]);
+		}
+
+		pipe = new ComposedBordered(new PipeShape(SA, SB), arcs[0], arcs[1]//, arcs[2],
+//				arcs[3], arcs[4]
+						);
+		// Log.e("pipe",""+pipe.getLeft()+","+pipe.getTop()+","+(pipe.getRight()-pipe.getLeft())+","+(pipe.getBottom()-pipe.getTop()));
+		float range10 = (1 + (float) Math.cos(elapsedTime / 300)) * 20;
+		// pipe.resize(10+range10,20+range10,413.333f,241.666f);
+		pipe.translate(5 - range10, 5 - range10);
+
+//		split.setBodyShader(new LinearGradient(split.getLeft(), split.getTop(),
+//				split.getRight(), split.getTop(), Color.RED, Color.BLACK,
+//				TileMode.CLAMP));
+
 		pipe.setEdgeColor(Color.RED);
-//		arc.setEdgeColor(Color.BLACK);
+		// arc.setEdgeColor(Color.BLACK);
 	}
 
 	protected void onDraw(Canvas canvas) {
@@ -201,10 +301,11 @@ public class GraphicTestView extends View {
 
 		buildDrawable();
 
+		drawGrid(canvas,50);
 		pipe.draw(canvas);
 
-		 if (isAnimationPending)
-		 this.postInvalidateDelayed(1000 / FPS);
+		if (isAnimationPending)
+			this.postInvalidateDelayed(1000 / FPS);
 	}
 
 	private void computePositions(float ratio) {
